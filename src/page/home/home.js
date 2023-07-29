48ae09a0653997a953d87cf7fdcb3a1a63616bf3
Nehal Garg <nehalgarg37@gmail.com>
2023-07-29 19:01:12 +0530

    Carousel

--- src/page/home/home.js
+++ src/page/home/home.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from "react"
 import "./home.css"
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+import { Link } from "react-router-dom";
 
 const Home = () => {
 
@@ -10,7 +13,38 @@ const Home = () => {
     }, [])
     return (
         <>
-            Home Page
+            <div className="poster">
+                <Carousel
+                    showThumbs = {false}
+                    autoPlay = {true}
+                    transitionTime = {3}
+                    infiniteLoop = {true}
+                    showStatus = {false}
+q               >
+                    {
+                        popularMovies.map(movie =>(
+                            <Link style = {{textDecoration: "none", color: "white"}} to= {`/movie/${movie.id}`} >
+                            <div className="posterImage">
+                                <img src={`https://image.tmdb.org/t/p/original${movie && movie.backdrop_path}`} alt="" />
+                            </div>
+                            <div className="posterImage__overlay">
+                                <div className="posterImage__title">
+                                    {movie ? movie.original_title: ""}
+                                <div className="posterImage__runtime">
+                                    {movie ? movie.release_date: ""}
+                                    <span className="posterImage__rating">
+                                        {movie ? movie.vote_average : ""}
+                                        <i className="fas fa-star"> {" "}</i>
+                                    </span>
+                                </div>
+                                <div className="posterImage__description">{movie? movie.overview: ""}</div>
+                                </div>
+                            </div>
+                            </Link>
+                        ))
+                    }
+                </Carousel>
+            </div>
         </>
     )
 }
